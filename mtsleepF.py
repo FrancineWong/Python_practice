6c93312b27917340512401ec1e4e696b4d8ec6af
王敏凡 <Fancy@wangminfandeMacBook-Pro.local>
2017-05-06 12:22:43 -0700

    with midify

--- mtsleepF.py
+++ mtsleepF.py
@@ -1,9 +1,10 @@
 #!/User/Fancy/desktop/Python_practice python3
-
+#utilize with to simplify the acquire and release of lock
 from atexit import register
 from random import randrange
 from threading import Thread, Lock, currentThread
 from time import sleep, ctime
+# from __future__ import with_statement #2.5 only
 
 class CleanOutputSet(set):
 	"""docstring for CleanOutputSet"""
@@ -16,16 +17,24 @@ remaining = CleanOutputSet()
 
 def loop(nsec):
 	myname = currentThread().name
-	lock.acquire()
-	remaining.add(myname)
-	print('[%s] Started %s' % (ctime(), myname))
-	lock.release()
+	# lock.acquire()
+	# remaining.add(myname)
+	# print('[%s] Started %s' % (ctime(), myname))
+	# lock.release()
+	# sleep(nsec)
+	# lock.acquire()
+	# remaining.remove(myname)
+	# print('[%s] Completed %s (%d secs)' % (ctime(), myname, nsec))
+	# print(' (remaining: %s)' % (remaining or 'NONE'))
+	# lock.release()
+	with lock:
+		remaining.add(myname)
+		print('[%s] Started %s' % (ctime(), myname))
 	sleep(nsec)
-	lock.acquire()
-	remaining.remove(myname)
-	print('[%s] Completed %s (%d secs)' % (ctime(), myname, nsec))
-	print(' (remaining: %s)' % (remaining or 'NONE'))
-	lock.release()
+	with lock:
+		remaining.remove(myname)
+		print('[%s] Completed %s (%d secs)' % (ctime(), myname, nsec))
+		print(' (remaining: %s)' % (remaining or 'NONE'))
 
 def _main():
 	for pause in loops:
@@ -37,3 +46,4 @@ def _atexit():
 
 if __name__ == '__main__':
 	_main()
+
