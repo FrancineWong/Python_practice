0a07076ccd00190b4fc5efee547ee803782644b9
王敏凡 <Fancy@wangminfandeMacBook-Pro.local>
2017-05-06 21:48:29 -0700

    button label combine

--- bookrank.py
+++ bookrank.py
@@ -7,17 +7,19 @@ from threading import Thread
 from time import ctime
 from urllib.request import urlopen as uopen
 
-REGEX = compile('#([\d,]+) in Books')
+REGEX = compile('#([\d,]+) in Books') # 匹配图书排名模式，正则表达
 AMZN = 'http://amazon.com/dp/'
 
-ISBNs = {
+ISBNs = { #国标标准书号
 	'0132269937': 'Core Python Programming',
 	'0132356139': 'Python Web Development with Django',
-	'0137143419': 'Python Fundamentals'
+	'0137143419': 'Python Fundamentals',
 }
 
 def getRanking(isbn):
-	page = uopen('%s%s' % (AMZN, isbn)) # or str.format()
+	# page = uopen('%s%s' % (AMZN, isbn)) 
+	# or str.format()
+	page = uopen('{}{}'.format(AMZN, isbn))
 	data = page.read()
 	page.close()
 	return REGEX.findall(data)[0]
@@ -28,7 +30,8 @@ def _showRanking(isbn):
 def _main():
 	print('AT', ctime(), 'on Amazon...')
 	for isbn in ISBNs:
-		_showRanking(isbn)
+		# _showRanking(isbn)
+		Thread(target=_showRanking, args=(isbn,)).start() #_showRanking(isbn)
 
 @register
 def _atexit():
