77f166bd1570863d0bdf64eea005c9c614f81f5e
王敏凡 <Fancy@wangminfandeMacBook-Pro.local>
2017-05-06 21:32:57 -0700

    label

---
+++ tkhello1.py
@@ -0,0 +1,13 @@
+#!/User/Fancy/desktop/Python_practice python
+import tkinter
+
+top = tkinter.Tk()
+label = tkinter.Label(top, text='Hello World!')
+label.pack()
+top.mainloop()
+
+# from tkinter import *
+# top = Tk()
+# label = Label(top, text='Hello World!')
+# label.pack()
+# top.mainloop()
